947d32cadd9acdac5ce36fdc14296e580fe1d2ea
Victor Bonnelle <victor.bonnelle@protonmail.com>
2022-07-27 23:21:24 +0200

    Fix second adj & probs conversion

--- utils/subject.py
+++ utils/subject.py
@@ -22,27 +22,27 @@ def get_subject():
 
     subject = []
 
-    is_verb = pr.Prob(int(config.get("probs.verb")))
+    is_verb = pr.Prob(float(config.get("probs.verb")))
 
     # Verb
     if (is_verb):
-        config.set("probs.verb", int(config.get("probs.verb")) - 0.1)
+        config.set("probs.verb", float(config.get("probs.verb")) - 0.1)
         subject.append(get("verb"))
 
         # Adverb
-        add_adverb = pr.Prob(int(config.get("probs.adverb")))
+        add_adverb = pr.Prob(float(config.get("probs.adverb")))
         if (add_adverb):
             subject.append(get("adverb"))
 
     # Noun
     else:
-        config.set("probs.verb", int(config.get("probs.verb")) + 0.1)
+        config.set("probs.verb", float(config.get("probs.verb")) + 0.1)
         
         noun = get("noun")
         subject.append(noun)
         
         # Adjective
-        add_adjective = pr.Prob(int(config.get("probs.adjective")))
+        add_adjective = pr.Prob(float(config.get("probs.adjective")))
         if (add_adjective):
             try:
                 form = inflect.get_word_attrs(noun)
@@ -58,12 +58,12 @@ def get_subject():
             subject.append(inflected_adj)
 
             # Second adjective
-            add_second_adjective = pr.Prob(int(config.get("probs.second_adjective")))
+            add_second_adjective = pr.Prob(float(config.get("probs.second_adjective")))
             if (add_second_adjective):
 
                 second_adjective = get("adjective")
                 try:
-                    inflected_second_adj = inflect.inflect_word(form)
+                    inflected_second_adj = inflect.inflect_word(second_adjective, form)
                 except WordNotInDictionaryError:
                     inflected_second_adj = second_adjective
                 
