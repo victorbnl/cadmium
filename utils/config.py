f58a7ceeb1fea675c39dde34102c8e3f8705d12a
Victor Bonnelle <victor.bonnelle@protonmail.com>
2022-07-27 22:27:09 +0200

    Update config utils to support dot notation

--- utils/config.py
+++ utils/config.py
@@ -1,19 +1,30 @@
 #-*- coding: utf-8 -*-
 
 import yaml
+from functools import reduce
 
 with open("data/config.yml", "r") as file_:
     config = yaml.safe_load(file_) or {}
 
-def get(key):
+def get(keys, dict_=config):
     """Get config item"""
 
-    return config[key]
+    if "." in keys:
+        key, rest = keys.split(".", 1)
+        return get(rest, dict_[key])
+    else:
+        return dict_[keys]
 
-def set(key, value):
+def set(keys, item, dict_=config):
     """Set config item"""
 
-    config[key] = value
+    if "." in keys:
+        key, rest = keys.split(".", 1)
+        if key not in dict_:
+            dict_[key] = {}
+        set(rest, item, dict_[key])
+    else:
+        dict_[keys] = item
 
     with open("data/config.yml", "w") as file_:
         file_.write(yaml.dump(config, allow_unicode=True))
