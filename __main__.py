8d59d6ed84ad5f7342c58cdd6657a95436c49fd3
Victor Bonnelle <victor.bonnelle@protonmail.com>
2022-07-28 17:58:24 +0200

    Default embed color

--- __main__.py
+++ __main__.py
@@ -27,6 +27,13 @@ prefix = getenv("PREFIX")
 intents = discord.Intents.default()
 intents.message_content = True
 
+default_send_embed = discord.abc.Messageable.send_embed
+async def send_embed(self, embed_dict, **kwargs):
+    if "color" not in embed_dict.keys():
+        embed_dict["color"] = int(config.get("color"), 16)
+    return await default_send_embed(self, embed_dict, **kwargs)
+discord.abc.Messageable.send_embed = send_embed
+
 class SubjectsBot(commands.Bot):
 
     async def block_other_guilds_check(self, ctx):
