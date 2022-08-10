73e8176ac71cf81055686ed69bf8e98222dad0ec
Victor Bonnelle <victor.bonnelle@protonmail.com>
2022-08-10 18:28:25 +0200

    Make i18n module a class

--- cadmium/i18n/i18n.py
+++ cadmium/i18n/i18n.py
@@ -1,22 +1,29 @@
 import os
 import yaml
 
-# Hardcoded for now as this bot is used on only one server for the moment and it's french
-# Meant to be edited and set dynamically if the bot grows
-lang = "fr"
+class I18n():
 
-with open(os.path.join(os.path.dirname(__file__), f"strings/{lang}.yml")) as file_:
-    strings = yaml.safe_load(file_)
+    def __init__(self):
 
+        # Hardcoded for now as this bot is used on only one server for the moment and it's french
+        # Meant to be edited and set dynamically if the bot grows
+        self.lang = "fr"
 
-def i18n(string, vars={}):
-    """Get i18n string."""
+        # Get strings
+        with open(os.path.join(os.path.dirname(__file__), f"strings/{self.lang}.yml")) as file_:
+            self.strings = yaml.safe_load(file_)
 
-    def get(keys, dict_=strings):
-        if "." in keys:
-            key, rest = keys.split(".", 1)
-            return get(rest, dict_[key])
-        else:
-            return dict_[keys]
+    def i18n(self, string, vars={}):
+        """Get i18n string."""
 
-    return get(string).format_map(vars)
+        def get(keys, dict_=self.strings):
+            if "." in keys:
+                key, rest = keys.split(".", 1)
+                return get(rest, dict_[key])
+            else:
+                return dict_[keys]
+
+        return get(string).format_map(vars)
+
+_inst = I18n()
+i18n = _inst.i18n
