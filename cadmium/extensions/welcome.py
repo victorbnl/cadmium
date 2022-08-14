743462b4cde95e0078c4845f2b066eb52e1b588e
Victor Bonnelle <victor.bonnelle@protonmail.com>
2022-08-15 01:14:45 +0200

    Better welcome module

--- cadmium/extensions/welcome.py
+++ cadmium/extensions/welcome.py
@@ -8,21 +8,28 @@ class Welcome():
     def __init__(self, bot):
         self.bot = bot
 
+    async def send_message(self, member, message):
+        """Send a message in the welcome channel."""
+
+        # Get channel
+        channel_id = int(config.get('welcome_channel'))
+        channel = self.bot.get_channel(channel_id)
+
+        # Format message
+        message = message.format(
+            mention=member.mention,
+            name=member.name
+        )
+
     async def on_member_join(self, member):
         """When a member joins the server."""
 
-        channel_id = int(config.get('welcome_channel'))
-        channel = self.bot.get_channel(channel_id)
-        message = config.get('welcome_message').format(mention=member.mention)
-        await channel.send(message)
+        await self.send_message(member, config.get('welcome_message'))
 
     async def on_member_remove(self, member):
         """When a member leaves the server."""
 
-        channel_id = int(config.get('welcome_channel'))
-        channel = self.bot.get_channel(channel_id)
-        message = config.get('goodbye_message').format(mention=member.mention)
-        await channel.send(message)
+        await self.send_message(member, config.get('goodbye_message'))
 
 
 def setup(bot):
